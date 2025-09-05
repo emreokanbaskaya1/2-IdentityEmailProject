20c0827aac0d6faa5aec5d74c463f3e85ad07fe5
EMRE OKAN BAŞKAYA <emreokanbaskaya@gmail.com>
2025-09-05 16:46:56 +0300

    Add project files.

---
+++ IdentityEmailApp/Entities/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityEmailApp.Entities
+{
+    public class AppUser : IdentityUser<int>
+    {
+        public string FirstName { get; set; }
+        public string  LastName { get; set; }
+        public string?  ImageUrl{ get; set; }
+        public ICollection<Message>  SentMessages { get; set; }
+        public ICollection<Message>  ReceivedMessages { get; set; }
+
+    }
+}
---
+++ IdentityEmailApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdentityEmailApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
